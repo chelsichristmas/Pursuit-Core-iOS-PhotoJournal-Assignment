77aa233cef730776dbe3186620d5e21d5d3d48d7
Chelsi Christmas <c.christmas95@gmail.com>
2020-05-04 22:59:04 -0400

    created new outlets, added function for configuring the view controller when an entry/post is available,

--- Photo-Journal/Photo-Journal/Controllers/CreateEntryViewController.swift
+++ Photo-Journal/Photo-Journal/Controllers/CreateEntryViewController.swift
@@ -19,21 +19,38 @@ class CreateEntryViewController: UIViewController {
     
     @IBOutlet weak var postTextView: UITextView!
     
+    @IBOutlet weak var photoLibraryButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var createAndSaveButton: UIBarButtonItem!
+    
+    
     weak var delegate: CreateEntryDelegate?
+    
     private let dataPersistence = PersistenceHelper(filename: "images.plist")
-    private var entry: EntryObject?
+    
+    public var entry: EntryObject? {
+        didSet {
+       configureEditViewController()
+        }
+    }
+    
     
     private var entryObjects = [EntryObject]()
     
     private let imagePickerController = UIImagePickerController()
     
-    private var selectedImage: UIImage? {
+    public var selectedImage: UIImage? {
         didSet {
             postImageView.image = selectedImage
 
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        
+    }
     
     
     
@@ -53,6 +70,20 @@ class CreateEntryViewController: UIViewController {
       }
     }
     
+    private func configureEditViewController() {
+        guard let entry = entry else {
+                   fatalError("No entry object")
+               }
+        DispatchQueue.main.async {
+                       self.postImageView.image = UIImage(data: entry.imageData)
+                       
+                       self.photoLibraryButton.isEnabled = false
+                       self.cameraButton.isEnabled = false
+                       self.createAndSaveButton.title = "Save"
+            
+            self.postImageView.alpha = 0.6
+                   }
+    }
     
     
     @IBAction func pressedPhotoLibrary(_ sender: Any) {
@@ -82,6 +113,8 @@ class CreateEntryViewController: UIViewController {
 
 
 @IBAction func createButtonPressed(_ sender: UIBarButtonItem) {
+    
+    if entry == nil {
     let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
     let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
     alertController.addAction(cancelAction)
@@ -96,6 +129,10 @@ class CreateEntryViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     } else {
         present(alertController, animated: true)
+    }
+    } else {
+        // update function
+        
     }
     
     
@@ -139,12 +176,6 @@ private func appendNewPhotoToCollection() {
     entryObjects.insert(entryObject, at: 0)
     
     print(" Entry Objects Count: \(entryObjects.count)")
-//    print(entryObjects.count)
-//    let indexPath = IndexPath(row: 0, section: 0)
-//
-//    let imagesViewController = ImagesViewController()
-//    guard let collectionView = imagesViewController.collectionView else { return }
-//    collectionView.insertItems(at: [indexPath])
    
     
     entry = entryObject
